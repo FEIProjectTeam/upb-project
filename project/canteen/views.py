4f00b117522dbc16a4d4915baef0859f6f60db19
Viktor Bojda <viktorbojda17@gmail.com>
2023-10-22 15:49:33 +0200

    feat: add encryption service and view with simple template

--- project/canteen/views.py
+++ project/canteen/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views.generic import TemplateView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
+
+from canteen.serializers import MealSerializer
+from canteen.services.encryption import (symmetric_encrypt, generate_rsa_keys, encrypt_with_rsa_pub_key,
+                                         decrypt_with_rsa_prv_key, symmetric_decrypt, get_hmac)
+from canteen.services.meals import get_all_meals
+
+
+class ListMealsApi(APIView):
+    def get(self, request):
+        data = MealSerializer(get_all_meals(), many=True).data
+        return Response(data, HTTP_200_OK)
+
+
+class EncryptView(TemplateView):
+    template_name = "encryption.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        orig_data = MealSerializer(get_all_meals(), many=True).data
+        orig_data = json.dumps(orig_data)
+        context["orig_data"] = orig_data
+        password = 'password'
+
+        symmetric_key, salt, iv, encrypted_data = symmetric_encrypt(orig_data, password)
+        context["encrypted_data"] = encrypted_data.hex()
+
+        orig_hmac = get_hmac(symmetric_key, encrypted_data)
+
+        private_key, public_key, pem = generate_rsa_keys()
+
+        encrypted_symmetric_key = encrypt_with_rsa_pub_key(public_key, symmetric_key)
+        context["encrypted_symmetric_key"] = encrypted_symmetric_key.hex()
+
+        decrypted_symmetric_key = decrypt_with_rsa_prv_key(private_key, encrypted_symmetric_key)
+
+        if orig_hmac == get_hmac(decrypted_symmetric_key, encrypted_data):
+            context["hmac_result"] = "Data is authentic and intact, proceed with decryption."
+        else:
+            context["hmac_result"] = "Data integrity check failed, consider the data compromised."
+
+        decrypted_data = symmetric_decrypt(decrypted_symmetric_key, iv, encrypted_data)
+        context["decrypted_data"] = decrypted_data.decode('utf-8')
+
+        return context
--- project/core/urls.py
+++ project/core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from canteen.views import ListMealsApi, EncryptView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/meals/', ListMealsApi.as_view()),
+    path('encrypt/', EncryptView.as_view())
 ]
