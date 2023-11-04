149cadb666fdd45dfce0a07c05dd3c0dfd9058c2
Shapeles <xcernicka@stuba.sk>
2023-11-04 12:48:25 +0100

    Added Registration and Login nehehe

--- project/core/urls.py
+++ project/core/urls.py
@@ -15,13 +15,50 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
 from canteen.views import ListMealsApi, EncryptView, GenerateDummyDatabaseApi
+
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+from users.views import CustomLoginView, ResetPasswordView, ChangePasswordView
+
+from users.forms import LoginForm
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/meals/", ListMealsApi.as_view()),
     path("api/generate/", GenerateDummyDatabaseApi.as_view()),
     path("encrypt/", EncryptView.as_view()),
-]
+    path("users/", include('users.urls')),
+
+    path('admin/', admin.site.urls),
+
+    path('', include('users.urls')),
+
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
+                                           authentication_form=LoginForm), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
+
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
